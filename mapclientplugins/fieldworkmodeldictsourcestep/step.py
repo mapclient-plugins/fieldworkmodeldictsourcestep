dc9a028d54a024a0cded8e4cd33942ae97206674
Hugh Sorby <h.sorby@auckland.ac.nz>
2021-06-04 13:22:51 +1200

    Update step.py

--- mapclientplugins/fieldworkmodeldictsourcestep/step.py
+++ mapclientplugins/fieldworkmodeldictsourcestep/step.py
@@ -100,7 +100,7 @@ class FieldworkModelDictSourceStep(WorkflowStepMountPoint):
         '''
         self._config.update(json.loads(string))
 
-        d = ConfigureDialog(self._main_window)
+        d = ConfigureDialog()
         d.setWorkflowLocation(self._location)
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
