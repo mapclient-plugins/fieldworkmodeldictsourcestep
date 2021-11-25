c4f96674bda84ad2fa30b23c1c24878a07949193
hsorby <h.sorby@auckland.ac.nz>
2021-11-26 11:50:58 +1300

    Update to use GIAS3.

--- mapclientplugins/fieldworkmodeldictsourcestep/step.py
+++ mapclientplugins/fieldworkmodeldictsourcestep/step.py
@@ -1,6 +1,6 @@
-'''
+"""
 MAP Client Plugin Step
-'''
+"""
 import json
 
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
@@ -8,14 +8,14 @@ from mapclientplugins.fieldworkmodeldictsourcestep.configuredialog import Config
 
 import os
 import configobj
-from gias2.fieldwork.field import geometric_field
+from gias3.fieldwork.field import geometric_field
 
 
 class FieldworkModelDictSourceStep(WorkflowStepMountPoint):
-    '''
+    """
     Skeleton step which is intended to be a helpful starting point
     for new steps.
-    '''
+    """
 
     def __init__(self, location):
         super(FieldworkModelDictSourceStep, self).__init__('Fieldwork Model Dict Source', location)
@@ -27,40 +27,38 @@ class FieldworkModelDictSourceStep(WorkflowStepMountPoint):
                       'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
                       'ju#fieldworkmodeldict'))
         # Config:
-        self._config = {}
-        self._config['identifier'] = ''
-        self._config['Config File'] = '.ini'
+        self._config = {'identifier': '', 'Config File': '.ini'}
 
         self._modelPathDict = None
         self._modelDict = None  # ju#fieldworkmodeldict
 
     def execute(self):
-        '''
+        """
         Add your code here that will kick off the execution of the step.
         Make sure you call the _doneExecution() method when finished.  This method
         may be connected up to a button in a widget for example.
-        '''
+        """
         # Put your execute step code here before calling the '_doneExecution' method.
         self._parseConfig()
         self._loadModels()
         self._doneExecution()
 
     def getPortData(self, index):
-        '''
+        """
         Add your code here that will return the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         provides port for this step then the index can be ignored.
-        '''
+        """
         return self._modelDict  # ju#fieldworkmodeldict
 
     def configure(self):
-        '''
+        """
         This function will be called when the configure icon on the step is
         clicked.  It is appropriate to display a configuration dialog at this
         time.  If the conditions for the configuration of this step are complete
         then set:
             self._configured = True
-        '''
+        """
         dlg = ConfigureDialog(self._main_window)
         dlg.setWorkflowLocation(self._location)
         dlg.identifierOccursCount = self._identifierOccursCount
@@ -75,29 +73,29 @@ class FieldworkModelDictSourceStep(WorkflowStepMountPoint):
         self._configuredObserver()
 
     def getIdentifier(self):
-        '''
+        """
         The identifier is a string that must be unique within a workflow.
-        '''
+        """
         return self._config['identifier']
 
     def setIdentifier(self, identifier):
-        '''
+        """
         The framework will set the identifier for this step when it is loaded.
-        '''
+        """
         self._config['identifier'] = identifier
 
     def serialize(self):
-        '''
+        """
         Add code to serialize this step to string.  This method should
         implement the opposite of 'deserialize'.
-        '''
+        """
         return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def deserialize(self, string):
-        '''
+        """
         Add code to deserialize this step from string.  This method should
         implement the opposite of 'serialize'.
-        '''
+        """
         self._config.update(json.loads(string))
 
         d = ConfigureDialog()
